ad6af0f8c707b21e3c4a33e35cb8c702f7ce1964
ispasdani <ispasdani14@gmail.com>
2025-12-11 11:40:23 +0100

    starting the work on the rezizable components  + toolbar + left sidebar + canvas

--- app/(diagram)/d/[id]/page.tsx
+++ app/(diagram)/d/[id]/page.tsx
@@ -1,5 +1,31 @@
+"use client";
+
+import { DivideX } from "@/components/general/divideX";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import Toolbar from "@/components/workbench/toolbar/toolbar";
+
 const Workspace = () => {
-  return <div>Workspaceee</div>;
+  return (
+    <div className="">
+      <Toolbar />
+      <DivideX />
+      <ResizablePanelGroup direction="horizontal">
+        <>
+          <ResizablePanel defaultSize={20} minSize={15} maxSize={35}>
+            <div>left sidevar here</div>
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+        </>
+        <ResizablePanel defaultSize={80} minSize={50}>
+          <div>Canvas here</div>
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </div>
+  );
 };
 
 export default Workspace;
---
+++ components/workbench/toolbar/toolbar.tsx
@@ -0,0 +1,13 @@
+const Toolbar = () => {
+  return (
+    <div
+      className="h-[7vh] w-full flex items-center justify-between px-3 py-3"
+      role="toolbar"
+      aria-label="Diagram editor toolbar"
+    >
+      Toolbar
+    </div>
+  );
+};
+
+export default Toolbar;
