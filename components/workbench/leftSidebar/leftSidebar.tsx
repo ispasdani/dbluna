01697afe4ca899301dc331e81b325f07066f026a
ispasdani14 <ispasdani14@gmail.com>
2025-12-14 22:39:30 +0200

    sidebar introduced

--- components/workbench/leftSidebar/leftSidebar.tsx
+++ components/workbench/leftSidebar/leftSidebar.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import {
+  Sidebar,
+  SidebarInset,
+  SidebarProvider,
+} from "@/components/ui/sidebar";
 
 const LeftSidebar = () => {
-  return <div>LeftSidebar</div>;
+  return (
+    <SidebarProvider>
+      <ResizablePanelGroup direction="horizontal" className="h-dvh">
+        {/* Left: Sidebar pane */}
+        <ResizablePanel defaultSize={18} minSize={14} maxSize={30}>
+          <Sidebar />
+        </ResizablePanel>
+
+        <ResizableHandle withHandle />
+
+        {/* Right: Main content pane */}
+        <ResizablePanel defaultSize={82} minSize={50}>
+          <SidebarInset className="h-full">
+            {/* Your header / page content */}
+            <div className="p-4">Main content</div>
+          </SidebarInset>
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </SidebarProvider>
+  );
 };
 
 export default LeftSidebar;
